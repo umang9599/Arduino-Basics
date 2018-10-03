dd5be1dd52595e9ea211c3027d0ea788cccd0337
Umang Maheshwari <32447924+umang9599@users.noreply.github.com>
2018-10-03 23:11:04 +0530

    Added comments and removed redundancies from the code

--- Akhilesh/accelerometer.ino
+++ Akhilesh/accelerometer.ino
@@ -1,15 +1,18 @@
-float xpin = A1; 
+//code for getting orientation from accelerometer using arduino
+
+float xpin = A1;           //we are taking float as datatype because we need value accuarte upto decimals.
 float ypin = A2;
 float zpin = A3;                 
-void setup(){
-  
-  Serial.begin(9600);
+void setup()
+{
+  Serial.begin(9600);             // baud rate for serial communication from accelerometer is 9600
   pinMode(A1, INPUT);
   pinMode(A2,INPUT);
   pinMode(A3,INPUT);
   }
 
-void loop(){
+void loop()
+{
   Serial.print(analogRead(xpin));
   Serial.print("\t");
   Serial.print(analogRead(ypin));

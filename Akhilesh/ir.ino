c87255d8683bac6fed6fd8d7cc565a4807243b3e
Umang Maheshwari <32447924+umang9599@users.noreply.github.com>
2018-10-09 00:30:03 +0530

    Updated code
    Rectified code to make it easy to read and added comments.

--- Akhilesh/ir.ino
+++ Akhilesh/ir.ino
@@ -1,20 +1,24 @@
-int a = 7;  
-int b = HIGH;  
-void setup(){
-  pinMode(led, OUTPUT);
-  pinMode(a, INPUT);
-  Serial.begin(9600);  
+int ir = 7;                         //set ir input pin to pin 7
+int b = HIGH;                       // This is a variable to store the value which we get from IR sensor 
+int led = 13;                       // we have taken led pin as 13 because pin 13 in arduino UNO has a built in LED
+void setup()
+{
+  pinMode(led, OUTPUT);          
+  pinMode(ir, INPUT);
+  Serial.begin(9600);               // we have set the baud rate forserial communication to 9600   
 }
-void loop(){
- 
-  b = digitalRead(a); 
-  if (b == LOW){
-    Serial.println("stop");
-    digitalWrite(led, HIGH);
-  }
-  else{
-    Serial.println("go");
-    digitalWrite(led, LOW);
-  }
-  delay(200);
+void loop()
+{
+   b = digitalRead(ir); 
+   if (b == LOW)                    // If IR sensor detects an obstacle it would give the value LOW or 0
+   {
+      Serial.println("STOP!!");
+      digitalWrite(led, HIGH);
+   }
+   else
+   {
+      Serial.println("GO");
+      digitalWrite(led, LOW);
+   }
+  delay(200);                  
 }

8f20212d1e15237a857f8859f3055c6e3c1a8d7f
Umang Maheshwari <32447924+umang9599@users.noreply.github.com>
2020-10-06 00:34:48 +0530

    Update flame_sensor.ino

--- Umang/flame_sensor.ino
+++ Umang/flame_sensor.ino
@@ -3,25 +3,26 @@ int sensorPin = A0; // select the input pin for the LDR
 int sensorValue = 0; // variable to store the value coming from the sensor
 int led = 9; // Output pin for LED
 int buzzer = 12; // Output pin for Buzzer
-void setup() { 
+void setup() 
+{ 
 // declare the ledPin and buzzer as an OUTPUT: 
-pinMode(led, OUTPUT); 
-pinMode(buzzer,OUTPUT); 
-Serial.begin(9600); 
+  pinMode(led, OUTPUT); 
+  pinMode(buzzer,OUTPUT); 
+  Serial.begin(9600); //9600 is the baud rate at which the communication between the arduino and the serial monitor of computer takes place. 
 } 
 void loop() 
 {  
-sensorValue = analogRead(sensorPin); 
-Serial.println(sensorValue); 
-if (sensorValue < 100)
-{ 
-Serial.println("Fire Detected"); 
-Serial.println("LED on"); 
-digitalWrite(led,HIGH); 
-digitalWrite(buzzer,HIGH); 
-delay(1000); 
-} 
-digitalWrite(led,LOW); 
-digitalWrite(buzzer,LOW); 
-delay(sensorValue); 
+  sensorValue = analogRead(sensorPin); 
+  Serial.println(sensorValue); 
+  if (sensorValue < 100)
+  { 
+    Serial.println("Fire Detected"); 
+    Serial.println("LED on"); 
+    digitalWrite(led,HIGH); 
+    digitalWrite(buzzer,HIGH); 
+    delay(1000); //This adds a delay or pauses the program for the time stated. The time is always stated in miliseconds 
+  } 
+  digitalWrite(led,LOW); 
+  digitalWrite(buzzer,LOW); 
+  delay(sensorValue); 
 }
